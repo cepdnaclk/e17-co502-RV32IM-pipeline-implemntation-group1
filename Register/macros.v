611b69a7aa5ec1d863a0eefcbfd54231f4f00d12
Dhushintha Ramalingam <126660228+Dhushintha-R@users.noreply.github.com>
2023-04-27 13:44:47 +0530

    Delete macros.v

--- Register/macros.v
+++
@@ -1,8 +0,0 @@
-`define assert(sig, val,op) \
-    if (sig !== val) begin \
-        $display("ASSERTION FAILED in %m: sig != %b [original value %b] in line %0d", val, sig, `__LINE__); \
-        $finish; \
-    end \
-$display("%s Passed!",op);
-
-     
